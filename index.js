74f978e3e8ac6bdca31fb6dd4066956506530467
Ibnul Ashir <ibnulashir42@gmail.com>
2022-12-27 13:38:48 +0600

    file update

--- index.js
+++ index.js
@@ -2,9 +2,32 @@
 const http = require('http')
 const { handleReqResponse } = require('./helpers/handleReqRes')
 const environment = require('./helpers/environments')
+const data = require('./lib/data')
 
 // module scafholding
 const app = {}
+
+// data testing
+// create
+/*
+data.create('testing', 'newFile', {name: 'newFile'}, (err) => {
+    console.log(err)
+})
+*/
+
+// read
+/*
+data.read('testing', 'newFile', (err, data) => {
+    console.log(err, data)
+})
+*/
+
+// update
+/*
+data.update('testing', 'newFile', {name: 'Bangladesh', language: "BN"}, (err) => {
+    console.log(err)
+})
+*/
 
 // configuration
 app.config={
