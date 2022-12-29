d66388fc4e480588983ff36c06b2905f3b98e536
Ibnul Ashir <ibnulashir42@gmail.com>
2022-12-29 13:33:17 +0600

    get an specific user

--- helpers/environments.js
+++ helpers/environments.js
@@ -1,23 +1,27 @@
 // module scaffholding
-const environments = {}
+const environments = {};
 
 environments.staging = {
-    port: 3000,
-    envName: 'staging',
-    secretKey: 'kjdfgvjkh4378bbv'
-}
+  port: 3000,
+  envName: "staging",
+  secretKey: "kjdfgvjkh4378bbv",
+};
 
 environments.production = {
-    port: 5000,
-    envName: 'production',
-    secretKey: 'kjdfgvjkh4378bbv'
-}
+  port: 5000,
+  envName: "production",
+  secretKey: "kjdfgvjkh4378bbv",
+};
 
 // check right environment
-const currentEnvironment = typeof(process.env.NODE_ENV) === 'string'? process.env.NODE_ENV: 'staging'
+const currentEnvironment =
+  typeof process.env.NODE_ENV === "string" ? process.env.NODE_ENV : "staging";
 
 // environment to export
-const exportEnvironment = typeof environments[currentEnvironment] === 'object' ? environments[currentEnvironment] : environments.staging
+const exportEnvironment =
+  typeof environments[currentEnvironment] === "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
 
 // export environment
-module.exports = exportEnvironment
+module.exports = exportEnvironment;
